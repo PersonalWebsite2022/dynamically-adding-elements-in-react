7754d607bcdceababd463126cd027eca8c83a76f
PersonalWebsite2022 <PersonalWebsite2022@gmail.com>
2022-11-28 21:57:22 -0800

    Dynamically Adding Elements in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  let names = ["Jason", "Bob", "Tom"];
+  function addElements(input){
+    let spans=[];
+    for(let i=0; i<input.length;i++)
+    {
+      let span = React.createElement("span", {className: "spanName", key: i.toString()},input[i]);
+      spans.push(span);
+    }
+    let mainContainer = React.createElement("div", {className: "mainContainer"}, spans)
+    return mainContainer;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {addElements(names)}
     </div>
   );
 }
